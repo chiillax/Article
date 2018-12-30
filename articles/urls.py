371e9bc9ccbab0aaf9440116f32e378c8078a2b9
chiillax <ak0852122@gmail.com>
2018-12-31 04:52:11 +0530

    added forget password button

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import ArticleListView, ArticleUpdateView, ArticleDetailView, ArticleDeleteView, ArticleCreateView
+from .views import ArticleListView, ArticleUpdateView, ArticleDetailView, ArticleDeleteView, ArticleCreateView, UserArticleListView
+# from django.conf.urls import url 
 
 urlpatterns = [
     path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
     path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('<author>/', UserArticleListView.as_view(), name="user_articles"),
+    # url(r'^([\w-]+)/$', UserArticleListView.as_view(), name="user_articles"),
     path('', ArticleListView.as_view(), name='article_list'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 
 class ArticleListView(LoginRequiredMixin, ListView):
@@ -52,3 +54,16 @@ class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         obj = self.get_object()
         return obj.author == self.request.user
+
+
+class UserArticleListView(LoginRequiredMixin, ListView):
+    model = Article
+    template_name = 'article/article_list.html'
+    login_url = 'login'
+    ordering = ['-date']
+
+    def get_queryset(self):
+        self.author = get_object_or_404(get_user_model(), username=self.kwargs['author'])
+        return Article.objects.filter(author=self.author)
+        # self.username = get_object_or_404(get_user_model(), username=self.args[0])
+        # return Article.objects.filter(author=self.username)
