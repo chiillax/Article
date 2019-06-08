86b56fd442cfba7c00e46c2b95f93e437ac80cd7
chiillax <ak0852122@gmail.com>
2019-06-08 14:32:34 +0530

    New updates for Heroku deployment

--- articles/models.py
+++ articles/models.py
@@ -22,7 +22,7 @@ class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     text = models.CharField(max_length=140)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
-    date = models.DateTimeField(auto_now_add=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, default=1)
 
     def __str__(self):
         return self.text
