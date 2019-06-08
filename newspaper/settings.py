63ee295424bc2df809ba711cd2b529a0e7169d7c
chiillax <ak0852122@gmail.com>
2019-06-09 00:35:03 +0530

    New updates for Heroku deployment

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -91,27 +91,27 @@ WSGI_APPLICATION = 'newspaper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-in_heroku = False
-if 'DATABASE_URL' in os.environ:
-    in_heroku = True
+# in_heroku = False
+# if 'DATABASE_URL' in os.environ:
+#     in_heroku = True
 
-import dj_database_url
-if in_heroku:
-    DATABASES = {'default': dj_database_url.config()}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# import dj_database_url
+# if in_heroku:
+#     DATABASES = {'default': dj_database_url.config()}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
 #     }
-# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
